673d5edaa5f15e05536a1d090bf991c6c834efd7
Your Name <you@example.com>
2021-01-14 15:49:15 -0500

    [show] add urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 
-
+# create a url
+# create an action (in views)
 
 # main_app/views.py
 
@@ -25,8 +28,31 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+###### CATS #######
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
 
     
